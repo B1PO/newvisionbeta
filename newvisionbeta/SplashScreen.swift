b462547345218627aa3bb0fe707def644c8b9c19
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-07-20 22:10:14 -0600

    Prueba Splash 1

--- newvisionbeta/SplashScreen.swift
+++ newvisionbeta/SplashScreen.swift
@@ -1,8 +1,43 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State var isActive : Bool = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    // Customise your SplashScreen here
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image("LogoNew")
+                        .resizable()
+                        .frame(width: 120, height:120)
+                        .cornerRadius(30.0)
+                    
+                    Text("Matzu")
+                        .font(Font.custom("Baskerville-Bold", size: 26))
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.00
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -11,3 +46,4 @@ struct SplashScreen_Previews: PreviewProvider {
         SplashScreen()
     }
 }
+
