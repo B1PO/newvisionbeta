0f83da52808c673cde3a1dfacc07da92e3f20f55
Pedro Prado <pedroprado1418@gmail.com>
2023-07-20 21:39:48 -0600

    Initial Commit

---
+++ newvisionbeta/newvisionbetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  newvisionbetaApp.swift
+//  newvisionbeta
+//
+//  Created by Pedro Prado on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct newvisionbetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
