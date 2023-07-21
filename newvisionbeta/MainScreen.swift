a394bcb5eda2187fb0c3c6ec1a6134f3734bf83e
Pedro Prado <pedroprado1418@gmail.com>
2023-07-20 22:19:41 -0600

    Modificación de MainsScreen

--- newvisionbeta/MainScreen.swift
+++ newvisionbeta/MainScreen.swift
@@ -3,8 +3,8 @@ import SwiftUI
 struct MainScreen: View {
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Hi, World!")
+            Text("Hi, World!")
         }
     }
 }
