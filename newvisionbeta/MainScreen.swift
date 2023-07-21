dce9cbe0b78be46f9afd72eb3a1ee7d0d78b5b88
Pedro Prado <pedroprado1418@gmail.com>
2023-07-20 21:58:50 -0600

    Editando el código

--- newvisionbeta/MainScreen.swift
+++ newvisionbeta/MainScreen.swift
@@ -2,7 +2,10 @@ import SwiftUI
 
 struct MainScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
