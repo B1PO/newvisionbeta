4125d011556216fff1ba31ad9d4c6a498a9708a5
Pedro Prado <pedroprado1418@gmail.com>
2023-07-21 23:20:31 -0600

    Creation of Color extensions by Pedro, don't forget

--- newvisionbeta/Color.swift
+++ newvisionbeta/Color.swift
@@ -1,8 +1,12 @@
-//
-//  Color.swift
-//  newvisionbeta
-//
-//  Created by Emilia Zuñiga Losada on 21/07/23.
-//
+import SwiftUI
 
-import Foundation
+extension Color {
+    
+    public static let backgroundColor: Color = Color(UIColor(red: 32/255, green: 32/255, blue: 60/255, alpha: 1.0))
+    public static let frameColor: Color = Color(UIColor(red: 48/255, green: 28/255, blue: 84/255, alpha: 1.0))
+    public static let buttonColor1: Color = Color(UIColor(red: 255/255, green: 40/255, blue: 191/255, alpha: 1.0))
+    public static let buttonColor2: Color = Color(UIColor(red: 255/255, green: 118/255, blue: 118/255, alpha: 1.0))
+    public static let offButtonColor: Color = Color(UIColor(red: 56/255, green: 36/255, blue: 92/255, alpha: 1.0))
+    public static let slideMenuColor: Color = Color(UIColor(red: 16/255, green: 4/255, blue: 37/255, alpha: 1.0))
+}
+
