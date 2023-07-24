f6672e83c0283970867718fea156885f9b3c11a5
Pedro Prado <pedroprado1418@gmail.com>
2023-07-23 20:59:45 -0600

    Recuperando lo perdido

---
+++ newvisionbeta/TabButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  TabButtonView.swift
+//  newvisionbeta
+//
+//  Created by Pedro Prado on 23/07/23.
+//
+
+import SwiftUI
+
+struct TabButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabButtonView()
+    }
+}
