d0b4ae2350b14d113fbc6b2c1c3ee6bd54bbf54a
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-07-26 23:50:11 -0600

    Vistas para el carousel

---
+++ newvisionbeta/CarouselViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  CarouselViewModel.swift
+//  newvisionbeta
+//
+//  Created by Emilia Zuñiga Losada on 26/07/23.
+//
+
+import SwiftUI
+
+struct CarouselViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarouselViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        CarouselViewModel()
+    }
+}
