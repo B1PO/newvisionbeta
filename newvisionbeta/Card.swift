22c2ac5dab96fc44e67fa9321d557ad97f1654da
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-07-29 22:09:42 -0600

    Carusel 1.2

--- newvisionbeta/Card.swift
+++ newvisionbeta/Card.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 //Model card
 struct Card: Identifiable {
-    var id = UUID() . uuidString
+    var id = UUID().uuidString
     var cardColor: Color
     var offset: CGFloat = 0
     var title: String
